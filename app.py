6312d28f5041ab856fda2e3cb44a0d4053051b08
TWU368 <125408506+TWU368@users.noreply.github.com>
2023-02-14 13:54:41 -0800

    updated

--- app.py
+++ app.py
@@ -13,6 +13,8 @@ df = pd.read_csv(ticker +".csv",parse_dates=['Date'], index_col=['Date'])
 begDate = df.index.min()
 endDate = df.index.max()
 
+pickStart = st.date_input("Pick start date:", begDate)
+pickEnd = st.date_input("Pick start date:", endDate)
 
 st.write(df)
 st.write(begDate)
